d22cb7ae9f35882f3688bf00ae0fc157be58ba94
Ian Thomson <i.thomson@ljmu.ac.uk>
2018-07-30 22:08:27 +0100

    added a link to the test page - initial set up

--- resources/views/frontpage.blade.php
+++ resources/views/frontpage.blade.php
@@ -1,6 +1,12 @@
 <html>
 
-<head><title>This is a test page</title></head>
+<head>
+    <title>This is a test page</title>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+
+</head>
 <body>
     <h1>TEST PAGE!</h1>
     This is a test !
