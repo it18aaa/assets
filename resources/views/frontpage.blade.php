bbf6941d51be19cc5a5b4f1562b920a4d9777af1
Ian Thomson <i.thomson@ljmu.ac.uk>
2018-08-01 09:17:53 +0100

    fixed frontpage

--- resources/views/frontpage.blade.php
+++ resources/views/frontpage.blade.php
@@ -11,10 +11,6 @@
     <h1>TEST PAGE!</h1>
     This is a test page :-)
     <br />
-<<<<<<< HEAD
     There is nothing to see here yet. You may go about your business.  Move along...
-=======
-    Nothing to see here yet. You may go about your business.  Move along...
->>>>>>> 4dfd5dbfdf0f067f8caf1bd9f5d6deb6d1b10b12
 </body>
 </html>
